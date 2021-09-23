dec3dd6dd8eb590fa73c74185fd4ab25734862e8
sebbASF <sebbASF@users.noreply.github.com>
2021-09-23 18:34:21 +0100

    Fetch calendar from SVN (#102)

---
+++ get_calendar.sh
@@ -0,0 +1,3 @@
+echo "Fetching calendar from SVN"
+cd content/foundation/board || exit
+/usr/bin/svn export https://svn.apache.org/repos/asf/infrastructure/site/trunk/content/foundation/board/calendar.md --force
