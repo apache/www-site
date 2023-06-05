39c2cc5a3d30f16d5bafe3a6ec3d82a125cfd010
Sebb <sebb@apache.org>
2023-06-05 12:19:19 +0100

    Further pagefind tests

--- get_calendar.sh
+++ get_calendar.sh
@@ -1,5 +1,3 @@
-source bin/activate
-pip3 list
 echo "Fetching calendar from SVN"
 cd content/foundation/board || exit
 /usr/bin/svn export https://svn.apache.org/repos/asf/infrastructure/site/trunk/content/foundation/board/calendar.md --force
