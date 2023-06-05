c0bbc2ab946826b911e11213f6639634f1d5b2e5
Sebb <sebb@apache.org>
2023-06-05 11:35:03 +0100

    Runs too late to affect processing

--- get_calendar.sh
+++ get_calendar.sh
@@ -1,19 +1,3 @@
-AUTOCONF='/tmp/www/pelican.auto.py'
-if [ -r ${AUTOCONF} ]
-then
-    if fgrep "TAGS_SAVE_AS = 'tags.html'" ${AUTOCONF}
-    then
-        echo "=======Already updated=============="
-    else
-        echo "=======Hack: enabling tags==============="
-        cat >>${AUTOCONF} <<EOD
-TAGS_SAVE_AS = 'tags.html'
-TAG_SAVE_AS = 'tag.html'
-EOD
-    fi
-else
-    echo "Cannot find ${AUTOCONF}"
-fi
 source bin/activate
 pip3 list
 echo "Fetching calendar from SVN"
