579bee6eb4ce3053631c3746321c70cd5cafd7b5
Sebb <sebb@apache.org>
2024-05-25 12:56:00 +0100

    Update to pagefind 1.1.0

--- pagefind.sh
+++ pagefind.sh
@@ -28,12 +28,13 @@ set -e # fast exit (must be done after 'which' invocation)
 
 if [ -z "$PAGEFIND" ] # could not find pagefind
 then
-    echo "Download pagefind"
-    PAGEFIND_VERSION='1.0.3'
-    PAGEFIND_HASH='e84ec7e8cb424022aa70ca14e6735c67c9728596753321dec782a902c3bfe6f0'
+    PAGEFIND_VERSION='1.1.0'
+    PAGEFIND_HASH='b20d0b06b54eb2fba1e290245919376364584a456fb644e66eecdb67549b1cf2'
+    echo "Download pagefind ${PAGEFIND_VERSION}"
     BINDIR=$(mktemp -d)
     TARGET=${BINDIR}/pagefind.tar.gz
-    wget --no-verbose -O ${TARGET} https://github.com/CloudCannon/pagefind/releases/download/v${PAGEFIND_VERSION}/pagefind-v${PAGEFIND_VERSION}-x86_64-unknown-linux-musl.tar.gz
+    OS_TYPE=x86_64-unknown-linux-musl
+    wget --quiet -O ${TARGET} https://github.com/CloudCannon/pagefind/releases/download/v${PAGEFIND_VERSION}/pagefind-v${PAGEFIND_VERSION}-${OS_TYPE}.tar.gz
     echo "${PAGEFIND_HASH}  ${TARGET}" > ${TARGET}.sha256
     if shasum -a 256 -c ${TARGET}.sha256
     then
