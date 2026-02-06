f913cb0a4dac7f33cff208c69a060c50fd68da70
buildbot <users@infra.apache.org>
2026-02-06 16:14:28 +0000

    Automatic Site Publish by Buildbot

--- output/js/slideshow.js
+++ output/js/slideshow.js
@@ -20,10 +20,16 @@ function showSlides(n) {
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none"; 
+    slides[i].classList.remove("active");
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].classList.remove("active")
   }
   slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+    // Use a timeout to allow the display change to take effect before adding the active class
+    setTimeout(() => {
+        slides[slideIndex-1].classList.add("active");
+        dots[slideIndex-1].classList.add("active");
+    }, 1);
+ 
 }
